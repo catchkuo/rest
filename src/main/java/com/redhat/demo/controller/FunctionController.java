5cc84ff73b1ce85a5204512a601d448727a2b922
catchkuo <kwkaki@gmail.com>
2021-09-28 16:26:19 +0800

    abc

--- src/main/java/com/redhat/demo/controller/FunctionController.java
+++ src/main/java/com/redhat/demo/controller/FunctionController.java
@@ -9,17 +9,22 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/fn")
 public class FunctionController {
+	
+	static RestTemplate restTemplate = new RestTemplate();
 
 //	http://localhost:8080/fn/fib?lv=44
+//	curl -d "lv=2" -X POST http://localhost:8080/fn/fib
 	@RequestMapping(value = "/fib", method = RequestMethod.GET,params = {"lv"})
 	public String runFibLv(@RequestParam(value = "lv") int lv,@RequestHeader Map<String, String> headers) throws UnknownHostException {
 
@@ -27,7 +32,7 @@ public class FunctionController {
 		Date date1 = new Date();
 		
 		
-		System.out.println("--------------------------------------------------");
+		System.out.println("GET--------------------------------------------------");
 		
 		headers.forEach((key, value) -> {
 			 System.out.println(String.format("Header '%s' = %s", key, value));
@@ -50,6 +55,43 @@ public class FunctionController {
 		return  difference.toString() + "ms";
 	}
 	
+	@RequestMapping(value = "/fib", method = RequestMethod.POST,params = {"lv"})
+	public String runPostFibLv(@RequestParam(value = "lv") int lv,@RequestHeader Map<String, String> headers) throws UnknownHostException {
+
+		SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+		Date date1 = new Date();
+		
+		
+		System.out.println("POST--------------------------------------------------");
+		
+		headers.forEach((key, value) -> {
+			 System.out.println(String.format("Header '%s' = %s", key, value));
+		    });
+		 
+		System.out.println("Start time:"+format.format(date1));
+		System.out.println( "My jost Name is :" + InetAddress.getLocalHost().getHostName());
+		System.out.println( "Lv:" +lv );
+		
+		System.out.println("--------------------------------------------------");
+		
+		 
+		
+
+		String fooResourceUrl
+		  = "http://localhost:8080/fn/fib?lv=1";
+		ResponseEntity<String> response
+		  = restTemplate.getForEntity(fooResourceUrl, String.class);
+		
+		System.out.println(response.getBody().toString());
+		
+
+		Date date2 = new Date();
+		System.out.println("End time:"+format.format(date2));
+		Long difference = date2.getTime() - date1.getTime(); 
+		
+		
+		return  difference.toString() + "ms";
+	}
 
 
 	   
